f9d6ea116875bdbfbb8a119feef0b781209db650
Marcos Souza Brito <njsu5@hotmail.com>
2022-12-15 16:35:55 -0300

    requisito11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,3 +1,3 @@
 db.produtos.find({
-  $or: [{ nome: { $not: { $eq: 'Big Mac' } } }, { nome: { $not: { $eq: 'McChicken' } } }]
+  nome: { $nin: ["Big Mac", "McChicken"] }
 }, { nome: 1, curtidas: 1, vendidos: 1, _id: 0 });
